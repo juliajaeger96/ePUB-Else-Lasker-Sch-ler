5480005b27f194eadd01d733f94607c831f20bd9
juliajaeger96 <63102325+juliajaeger96@users.noreply.github.com>
2021-06-11 10:54:39 +0200

    epub

---
+++ result/OEBPS/Misc/button_function.js
@@ -0,0 +1,39 @@
+//<![CDATA[
+function Adressen() {
+          var elms = document.querySelectorAll("#myDIV");
+          
+          for (var i = 0; i < elms.length; i++) {
+
+            if (elms[i].style.display === "none") {
+              elms[i].style.display = "block";
+            } else {
+              elms[i].style.display = "none";
+            }
+          }
+        }
+function Poststempel() {
+          var elms = document.querySelectorAll("#stemp");
+          
+          for (var i = 0; i < elms.length; i++) {
+
+            if (elms[i].style.display === "none") {
+              elms[i].style.display = "block";
+            } else {
+              elms[i].style.display = "none";
+            }
+          }
+        }
+function Briefmarken() {
+          var elms = document.querySelectorAll("#circle");
+          
+          for (var i = 0; i < elms.length; i++) {
+
+            if (elms[i].style.display === "none") {
+              elms[i].style.display = "block";
+            } else {
+              elms[i].style.display = "none";
+            }
+          }
+        }
+                     //]]>  
+                    
