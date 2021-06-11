9acc46c7af7f7e8aaff73e1ea87b21076917b9e0
juliajaeger96 <63102325+juliajaeger96@users.noreply.github.com>
2021-06-11 10:59:22 +0200

    d

--- result/rom_zip4epub.bat
+++
@@ -1,5 +0,0 @@
-C:\Programme\7-Zip\7z a -tzip -mx=0 ".\epub_els.zip" ".\result\mimetype"
-C:\Programme\7-Zip\7z a -tzip ".\epub_els.zip" ".\result\OEBPS\"
-C:\Programme\7-Zip\7z a -tzip ".\epub_els.zip" ".\result\META-INF\"
-rename ".\epubcheck-4.2.5\epub_els.zip" epub_els.epub
-pause
